359bfb3d446114e0b6bc689782d92f5e71cce85c
vamsheerudra <30297253+vamsheerudra@users.noreply.github.com>
2023-04-06 17:41:55 +0530

    Add files via upload

---
+++ Install_NotepadPP.ps1
@@ -0,0 +1,17 @@
+﻿# Set the URL for the Notepad++ download
+$notepadppDownloadUrl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.2/npp.8.2.2.Installer.x64.exe"
+
+# Set the path for the Notepad++ installer file
+$notepadppInstallerPath = "$($env:TEMP)\npp.8.2.2.Installer.x64.exe"
+
+# Download the Notepad++ installer file
+Invoke-WebRequest -Uri $notepadppDownloadUrl -OutFile $notepadppInstallerPath
+
+# Define the Notepad++ installation arguments
+$notepadppArgs = "/S", "/D=`"$($env:ProgramFiles)\Notepad++`"", "/NOICONS", "/NORESTART", "/COMPONENTS=""scintilla,nppFTP,Explorer,Updater,Git,PluginManager,Localization""", "/TASKS=""!npp""", "/DONTRESTART"
+
+# Install Notepad++ with the custom configuration options
+Start-Process -FilePath $notepadppInstallerPath -ArgumentList $($notepadppArgs -join ' ') -Wait
+
+# Clean up the Notepad++ installer file
+Remove-Item $notepadppInstallerPath
